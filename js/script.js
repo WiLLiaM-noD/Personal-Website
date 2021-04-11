f9ef40c6f4547fbb235ad0d7ea5cd2b26f153dd1
WiLLiaM-noD <williamkurniawan1144@gmail.com>
2021-04-11 14:02:10 +0700

    fixing js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// Year in Website
 const year = new Date().getFullYear();
 const yearContainer = document.getElementById('year');
 yearContainer.textContent = year;
+
+// Scroll Effect
+const arr = document.querySelectorAll('a');
+const links = [];
+arr.forEach(el => {
+   if (el.classList.contains('nav-link')) links.push(el);
+});
+
+links.forEach(link => {
+   let target = document.getElementById(link.href);
+   target.addEventListener('click', function (e) {
+      e.preventDefault();
+      let element = document.getElementById(link.href);
+      element.scrollIntoView({ behavior: 'smooth' });
+   });
+});
